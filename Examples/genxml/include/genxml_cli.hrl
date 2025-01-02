b855e35c6a211ab903c587cc7d74b3743c7d67b3
Fred Youhanaie <fyrlang@anydata.co.uk>
2025-01-02 23:44:18 +0000

    remove the unused option

--- Examples/genxml/include/genxml_cli.hrl
+++ Examples/genxml/include/genxml_cli.hrl
@@ -3,8 +3,6 @@
         [ #{ name => verbose, long => "-verbose", short => $v,
              type => boolean, action => count,
              help => "be verbose, can use multiple times for warning to debug" },
-          #{ name => help, long => "-help", short => $h,
-             type => boolean,
              help => "display help/usage information" },
           #{ name => file, nargs => 1 }
         ] ).
