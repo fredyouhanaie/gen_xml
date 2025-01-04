853cc462598ff78bbeb8479f230e2fb087778b9f
Fred Youhanaie <fyrlang@anydata.co.uk>
2025-01-04 17:46:17 +0000

    enable the test macros to be used in other test modules

--- test/gen_xml_tests.erl
+++ test/gen_xml_tests.erl
@@ -3,7 +3,9 @@
 %%% @copyright (C) 2024, Fred Youhanaie
 %%% @doc
 %%%
-%%% Run the EUnit tests for the pnml module.
+%%% Run the EUnit tests for the gen_xml module.
+%%%
+%%% we use the `genxml_null' callback module for some of the tests.
 %%%
 %%% @end
 %%% Created : 2024-10-13 by Fred Youhanaie <fyrlang@anydata.co.uk>
@@ -12,12 +14,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(Doc_nofile, "test/no_file.xml").   %% this file should never exist!
--define(Doc_empty,  "test/empty_doc.xml"). %% keep this file empty
-
--define(Doc_sample_4, "test/sample-xml-files-sample-4.xml").
--define(Doc_sample_5, "test/sample-xml-files-sample-5.xml.gz").
--define(Doc_sample_6, "test/sample-xml-files-sample-6.xml.gz").
+-include_lib("gen_xml_tests.hrl").
 
 %% Change `Log_level' if investigating failed tests
 -define(Log_level, critical).
---
+++ test/gen_xml_tests.hrl
@@ -0,0 +1,6 @@
+-define(Doc_nofile, "test/no_file.xml").   %% this file should never exist!
+-define(Doc_empty,  "test/empty_doc.xml"). %% keep this file empty
+
+-define(Doc_sample_4, "test/sample-xml-files-sample-4.xml").
+-define(Doc_sample_5, "test/sample-xml-files-sample-5.xml.gz").
+-define(Doc_sample_6, "test/sample-xml-files-sample-6.xml.gz").
